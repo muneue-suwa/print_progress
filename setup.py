1940da8833f85f20aa0fadc78be15cb5e7316d0f
Muneue SUWA <dongsiku@gmail.com>
2020-12-20 12:07:15 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='print-progress',
+    version='1.0.0',
+    description='Print progress',
+    author='Muneue SUWA',
+    author_email='dongsiku@gmail.com',
+    url='https://github.com/dongsiku/print_progress',
+)
